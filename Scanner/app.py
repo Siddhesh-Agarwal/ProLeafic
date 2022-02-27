7880ccf3ddd30ab85b14cbc00aaaa474b2e1d7b0
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2022-02-27 19:55:50 +0530

    Add files via upload

--- Scanner/app.py
+++ Scanner/app.py
@@ -24,37 +24,54 @@ def import_and_predict(image_data, model):
 
 st.title("Plant Disease Identification")
 
-file = st.file_uploader(
-    label="Select picture of plant disease",
-    type=["jpg", "png"],
-    accept_multiple_files=False,
-)
+choice = st.selectbox("Choose an option", ["Scan", "Upload"])
 
-if st.button("Search for plant disease"):
-    if file is None:
-        st.warning("Select an Image (png/jpg) to continue. ")
-    else:
-        image = Image.open(file)
-        model = load_model()
-        st.image(image, use_column_width=True)
-        predictions = import_and_predict(image, model)
-        class_names = [
-            "Bacterial Spots",
-            "Healthy",
-            "Early Blight",
-            "Healthy",
-            "Late Blight",
-            "Target Spots",
-            "Mosaic Virus",
-            "Yellow Leaf Curl Virus",
-            "Bacterial Spots",
-            "Early Blight",
-            "Healthy" "Late Blight",
-            "Leaf Mold",
-            "Leaf Spots" "Spider Mites",
-        ]
-        disease = class_names[np.argmax(predictions)]
-        if disease == "Healthy":
-            st.success("This crop looks disease free!")
+if choice == "Upload":
+    file = st.file_uploader(
+        label="Select picture of plant disease",
+        type=["jpg", "png"],
+        accept_multiple_files=False,
+    )
+    if st.button("Search for plant disease"):
+        if file is None:
+            st.warning("Select an Image (png/jpg) to continue. ")
         else:
-            st.warning("This disease looks like", disease)
+            image = Image.open(file)
+            model = load_model()
+            st.image(image, use_column_width=True)
+            predictions = import_and_predict(image, model)
+            class_names = [
+                "Bacterial Spots",
+                "Healthy",
+                "Early Blight",
+                "Healthy",
+                "Late Blight",
+                "Target Spots",
+                "Mosaic Virus",
+                "Yellow Leaf Curl Virus",
+                "Bacterial Spots",
+                "Early Blight",
+                "Healthy",
+                "Late Blight",
+                "Leaf Mold",
+                "Leaf Spots",
+                "Spider Mites"
+            ]
+
+            disease = class_names[np.argmax(predictions)]
+
+            if disease == "Healthy":
+                st.success("This crop looks disease free!")
+            else:
+                st.warning("This disease looks like", disease)
+else:
+    file = st.camera_input("Click a picture of plant disease.")
+    if st.button("Search for plant disease"):
+        if file is None:
+            st.warning("Select an Image (png/jpg) to continue.")
+        else:
+            image = Image.open(file)
+            model = load_model()
+            st.image(image, use_column_width=True)
+            predictions = import_and_predict(image, model)
+            st.success("There is no disease on this plant.")
